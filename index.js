bcfe30ff16876fcfbb93c13340bdc6641606f494
PradeepaBharathi <bharathi83201@gmail.com>
2023-09-27 09:36:14 +0530

    updated

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import express from "express";
 
 import { user_router } from "./router/user_router.js";
 import { blog_router } from "./router/blog_router.js";
-import cors from 'cors'
+import cors from "cors";
 
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000', // Update with your client's URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, // Allow cookies and headers to be included
-}))
-app.use(express.json())
+app.use(
+  cors({
+    origin: "https://calm-squirrel-be28ab.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-app.use('/', user_router)
-app.use('/', blog_router)
+app.use("/", user_router);
+app.use("/", blog_router);
 
-app.get('/', (req, res) => {
-    res.send("first")
-})
+app.get("/", (req, res) => {
+  res.send("first");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server connceted to PORT ${PORT}`)
-})
-
-
+  console.log(`Server connceted to PORT ${PORT}`);
+});
